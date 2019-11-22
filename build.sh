5d43f80d3e7942ffdd1cbd1186c26fedaf32eda4
jaenek <jaenek@protonmail.com>
2019-11-22 22:54:05 +0100

    Use mcopy insted of mounting

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Set threads count used during GCC compilation
-THREADS_COUNT=4
-TEMP="/tmp/temp_disk_A"
-
-mkdir -p build/floppy/ENV
+THREADS_COUNT=[THREAD_COUNT]
+WORKSPACE_DIR="$PWD"
 
 # Delete old ELF and BIN files
 find build -iname "*.elf" -type f -delete
@@ -13,83 +11,82 @@ find environment -iname "*.elf" -type f -delete
 find library -iname "*.elf" -type f -delete
 find os -iname "*.elf" -type f -delete
 
-# Delete broken directory
-rm -rf $TEMP
-
 export MODE=release
 
 # Build bootloader
 cd os/bootloader
-    make $1 -j $THREADS_COUNT
+	make $1 -j "$THREADS_COUNT"
 
-    if [ "$?" != "0" ]; then
-        exit 1
-    fi
+	if [ "$?" != "0" ]; then
+		exit 1
+	fi
 cd ../..
 
 # Build C Library
 cd library
-    make $1 -j $THREADS_COUNT
+	make $1 -j "$THREADS_COUNT"
 
-    if [ "$?" != "0" ]; then
-        exit 1
-    fi
+	if [ "$?" != "0" ]; then
+		exit 1
+	fi
 cd ..
 
 # Build applications
 cd environment
-    for i in *;
-    do
-        cd $i
-        make $1 -j $THREADS_COUNT
+	for i in *;
+	do
+		cd "$i"
+		make $1 -j "$THREADS_COUNT"
 
-        if [ "$?" != "0" ]; then
-            exit 1
-        fi
+		if [ "$?" != "0" ]; then
+			exit 1
+		fi
 
-        if [ "$1" != "clean" ]; then
-            cp bin/$i.elf ../../build/floppy/ENV/`echo $i | tr a-z A-Z`.ELF
-        fi
+		if [ "$1" != "clean" ]; then
+			mkdir -p ../build/floppy/ENV
+			cp bin/"$i".elf ../../build/floppy/ENV/`echo "$i" | tr a-z A-Z`.ELF
+		fi
 
-        cd ..
-    done
+		cd ..
+	done
 cd ..
 
 # Build kernel
 cd os/kernel
-    make $1 -j $THREADS_COUNT
+	make $1 -j "$THREADS_COUNT"
 
-    if [ "$?" != "0" ]; then
-        exit 1
-    fi
+	if [ "$?" != "0" ]; then
+		exit 1
+	fi
 
-    if [ "$1" != "clean" ]; then
-        cp bin/kernel.bin ../../build/floppy/KERNEL.BIN
-        cp bin/kernel.elf ../../build/kernel.elf
-    fi
+	if [ "$1" != "clean" ]; then
+		cp bin/kernel.bin ../../build/floppy/KERNEL.BIN
+		cp bin/kernel.elf ../../build/kernel.elf
+	fi
 cd ../..
 
+copy() {
+	cd "$1"
+	for i in *;
+	do
+		mcopy -bvi "$FLOPPY_IMG" $i ::$i
+	done
+	cd "$WORKSPACE_DIR"
+}
+
+# Create floppy
 if [ "$1" != "clean" ]; then
+	FLOPPY_IMG="$WORKSPACE_DIR/build/floppy.img"
 	# Remove old floppy img
-	rm build/floppy.img
+	rm "$FLOPPY_IMG"
 
-	# Make and format the floppy disk
-	mkfs.msdos -C build/floppy.img 1440
+	# Make and format the floppy
+	mkfs.msdos -C "$FLOPPY_IMG" 1440
 
-    # Upload bootloader to the floppy
-    dd if=os/bootloader/bin/bootloader.bin of=build/floppy.img bs=512 conv=notrunc
+	# Upload bootloader to the floppy
+	dd if=os/bootloader/bin/bootloader.bin of="$FLOPPY_IMG" bs=512 conv=notrunc
 
-    # Copy kernel to the floppy
-	cd build
-	mkdir -p $TEMP
-	sudo mount -o loop,uid=$UID floppy.img $TEMP
-	cd floppy
-	cp -r . $TEMP
-	cd ../..
-	cd resources
-	cp -r . $TEMP
-    cd ..
-
-	# Unmount floppy
-	sudo umount $TEMP
+	# Copy kernel to the floppy
+	copy build/floppy
+	copy resources
 fi
