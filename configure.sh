1df0bd7370f8eea0459471f9e5e84863d01d647b
jaenek <jaenek@protonmail.com>
2019-11-23 04:08:27 +0100

    Added main configuration script

---
+++ configure.sh
@@ -0,0 +1,70 @@
+#/bin/sh
+
+THREAD_COUNT=${THREAD_COUNT:-1}
+
+help() {
+	echo "You must specify workspace directory like so:"
+	echo -e "\t./configure.sh <workspace directory>\n"
+	echo "You can also specify the thread count to use when compiling MicrOS:"
+	echo -e "\t./configure.sh <workspace directory> THREAD_COUNT=<number of threads>\n"
+}
+
+if [ "$1" = "help" ];
+then
+	help
+	exit 0
+fi
+
+if [ ! "$1" ];
+then
+	echo "Error: Please specify workspace directory"
+	help
+	exit 1
+else
+	WORKSPACE_DIR="$(readlink -f "$1")"
+	if [ ! -d $WORKSPACE_DIR ];
+	then
+		echo "Error: $WORKSPACE_DIR does not exist."
+	fi
+fi
+
+# Check for dependencies
+NASM="$(which nasm)"
+MCOPY="$(which mcopy)"
+
+if [ ! "$NASM" ];
+then
+	echo "Error: dependecies not met, nasm is not installed. Exitting..."
+	exit 1
+fi
+
+if [ ! "$MCOPY" ];
+then
+	echo "Error: dependecies not met, mcopy is not installed. Exitting..."
+	exit 1
+fi
+
+# Download files
+TEMP="/tmp/MicrOS_DevTools_temp"
+SRC="$TEMP/MicrOS-DevTools-1.0"
+CROSS="$TEMP/cross.tar.gz"
+mkdir "$TEMP"
+curl -Lk https://github.com/jaenek/MicrOS-DevTools/archive/v1.0.tar.gz | tar xzC "$TEMP"
+curl -Lk https://github.com/jaenek/MicrOS-DevTools/releases/download/v1.0/cross.tar.gz | sudo tar xzf "$CROSS" -C "/opt"
+
+# Replace strings
+sed -i "s/\[THREAD_COUNT\]/$THREAD_COUNT/g" "$SRC/build.sh"
+
+# Save to workspace directory
+mkdir -p "$WORKSPACE_DIR/scripts/"
+mv "$SRC/build.sh" "$WORKSPACE_DIR/scripts/"
+mkdir -p "$WORKSPACE_DIR/.vscode/"
+mv "$SRC/launch.json" "$WORKSPACE_DIR/.vscode/"
+mv "$SRC/tasks.json" "$WORKSPACE_DIR/.vscode/"
+
+# Create symlink to nasm
+mkdir -p "$WORKSPACE_DIR/tools/"
+ln -sf $NASM "$WORKSPACE_DIR/tools/nasm"
+
+# Remove temporary directory
+rm -r $TEMP
